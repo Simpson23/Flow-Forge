af0ed5b7c37569f6db3c339d7eed5e0175054958
safyan <safyyy6@gmail.com>
2025-08-03 20:06:11 +0100

    stripe fix and env error fix

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-06-30.basil' as const,
-});
-
 export async function POST(req: Request) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: '2025-06-30.basil' as const,
+  });
+
   const { plan } = await req.json();
 
   let priceId;
